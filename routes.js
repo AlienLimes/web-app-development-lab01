ba5edee008bd0ce7e6768e0a4c3e3d2da9204228
AlienLimes <kriszti145@gmail.com>
2026-04-29 08:56:21 +0100

    user authentication added

--- routes.js
+++ routes.js
@@ -10,10 +10,12 @@ import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
 import playlist from './controllers/playlist.js';
 import stats from './controllers/stats.js';
+import accounts from './controllers/accounts.js';
 
 
 
-router.get('/', start.createView);
+router.get('/start', start.createView);
+
 router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
 router.get('/playlist/:id', playlist.createView);
@@ -22,6 +24,12 @@ router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 router.get('/searchCategory', dashboard.createView);
 router.get('/sortData', dashboard.createView);
 
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 
 
 router.post('/playlist/:id/addsong', playlist.addSong);
--- server.js
+++ server.js
@@ -5,12 +5,19 @@ import logger from "./utils/logger.js";
 import routes from './routes.js'; 
 import { create } from 'express-handlebars';
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(cookieParser());
+
+
 
 const handlebars = create({
   extname: '.hbs', 
@@ -44,5 +51,7 @@ app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
 app.use("/", routes);
+
+
 
 app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
